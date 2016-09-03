f0423c5d28009aac82c14bf94a0fd12466748c55
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2016-09-03 21:52:38 +0400

    Add global hotkey handlers.

--- src/Tomighty/Core/TYDefaultTomighty.m
+++ src/Tomighty/Core/TYDefaultTomighty.m
@@ -5,10 +5,12 @@
 //  http://www.apache.org/licenses/LICENSE-2.0.txt
 //
 
+#import <Carbon/Carbon.h>
 #import "TYDefaultTimerContext.h"
 #import "TYDefaultTomighty.h"
 #import "TYEventBus.h"
 #import "TYPreferences.h"
+#import "MVTHotkey.h"
 
 @implementation TYDefaultTomighty
 {
@@ -17,6 +19,7 @@
     id <TYTimer> timer;
     id <TYPreferences> preferences;
     id <TYEventBus> eventBus;
+    EventHotKeyRef startHotkeyRef, stopHotkeyRef;
 }
 
 - (id)initWith:(id <TYTimer>)aTimer
@@ -35,6 +38,13 @@
         {
             [self incrementPomodoroCount];
         }];
+
+        [eventBus subscribeTo:PREFERENCE_CHANGE subscriber:^(id eventData) {
+            [self registerHotkeys];
+        }];
+
+        [self installHotkeyEventHandler];
+        [self registerHotkeys];
     }
     return self;
 }
@@ -98,6 +108,67 @@
     }
     
     [self setPomodoroCount:newCount];
+}
+
+// From here: http://stpeterandpaul.ca/tiger/documentation/Carbon/Reference/Carbon_Event_Manager_Ref/Reference/reference.html
+- (void)installHotkeyEventHandler
+{
+    EventTypeSpec eventType;
+    eventType.eventClass=kEventClassKeyboard;
+    eventType.eventKind=kEventHotKeyPressed;
+
+    InstallEventHandler(GetApplicationEventTarget(), &TYHotkeyHandler,
+                        1, &eventType, (__bridge void*)self, NULL);
+}
+
+- (void)unregisterKeys
+{
+    if(startHotkeyRef) {
+        UnregisterEventHotKey(startHotkeyRef);
+        startHotkeyRef = nil;
+    }
+    if(stopHotkeyRef) {
+        UnregisterEventHotKey(stopHotkeyRef);
+        stopHotkeyRef = nil;
+    }
+}
+
+- (void)registerHotkeys
+{
+    MVTHotkey *start = [MVTHotkey hotkeyWithString:[preferences
+                                                 getString:PREF_HOTKEY_START]];
+    MVTHotkey *stop = [MVTHotkey hotkeyWithString:[preferences
+                                                    getString:PREF_HOTKEY_STOP]];
+    EventHotKeyID hotkeyID;
+
+    [self unregisterKeys];
+
+    hotkeyID.signature='thk1'; // it's a UInt32 actually, value can be anything
+    hotkeyID.id=11;
+    RegisterEventHotKey(start.code, start.carbonFlags, hotkeyID,
+                        GetApplicationEventTarget(), 0, &startHotkeyRef);
+
+    hotkeyID.signature='thk2';
+    hotkeyID.id=13;
+    RegisterEventHotKey(stop.code, stop.carbonFlags, hotkeyID,
+                        GetApplicationEventTarget(), 0, &stopHotkeyRef);
+
+}
+
+OSStatus TYHotkeyHandler(EventHandlerCallRef next, EventRef evt, void *data) {
+    TYDefaultTomighty *target = (__bridge TYDefaultTomighty*)data;
+    EventHotKeyID hkid;
+    GetEventParameter(evt, kEventParamDirectObject,typeEventHotKeyID, NULL,
+                      sizeof(hkid), NULL, &hkid);
+    switch(hkid.id) {
+        case 11:
+            [target startPomodoro];
+            break;
+        case 13:
+            [target stopTimer];
+            break;
+    }
+    return noErr;
 }
 
 @end
