1f1beb366ac97752e3cf414240123fff21012fa8
Devon Tucker <dtucker@boundlessgeo.com>
2016-09-06 11:17:28 -0700

    Small fixes to the package script

--- build/package.sh
+++ build/package.sh
@@ -3,12 +3,15 @@
 #Us the create-dmg project to create the DMG
 #git clone https://github.com/andreyvit/yoursway-create-dmg.git
 
-VERSION=1.1
+VERSION="1.1"
+
+mkdir -p target/package
+cp -rf target/Tomighty.app target/package/
 
 yoursway-create-dmg/create-dmg \
-    --volname "Tomighty-$(VERSION)" \
+    --volname "Tomighty-$VERSION" \
     --icon-size 75 \
     --app-drop-link 10 10 \
     --icon target/package/Tomighty.app 95 10 \
-    target/Tomighty-$(VERSION).dmg \
+    "target/Tomighty-$VERSION.dmg" \
     target/package
